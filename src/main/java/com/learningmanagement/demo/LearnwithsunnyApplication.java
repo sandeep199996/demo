fcbfd41bac91744a622f2a27f038fedbe2923e96
sandeep199996 <medipellisandeep1@gmail.com>
2025-10-19 18:53:47 +0530

    to be intigrated

--- src/main/java/com/learningmanagement/demo/LearnwithsunnyApplication.java
+++ src/main/java/com/learningmanagement/demo/LearnwithsunnyApplication.java
@@ -1,11 +1,14 @@
 package com.learningmanagement.demo;
 
 import com.learningmanagement.demo.controller.initiation;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableCaching
 public class LearnwithsunnyApplication {
 
 	public static void main(String[] args) {
